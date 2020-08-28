ddfde911a990dc4732aeef2e43dd5ad1f464656d
ahmadnajjar90 <a7mad.najjar@hotmail.com>
2020-08-28 17:15:36 +0200

    carousel

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 15,
+        autoplay: true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    })
+})
