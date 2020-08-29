a85e27f53b61b9fb7198dc9132d0052ca1a0e131
ahmadnajjar90 <a7mad.najjar@hotmail.com>
2020-08-29 19:13:04 +0200

    carousel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
 
-    var owl = $('.owl-carousel');
+    var owl = $('#owl');
     owl.owlCarousel({
         items: 4,
+        loop: true,
+        margin: 15,
+        autoplay: true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    })
+
+    var owl2 = $('#owl2');
+    owl2.owlCarousel({
+        items: 5,
         loop: true,
         margin: 15,
         autoplay: true,
